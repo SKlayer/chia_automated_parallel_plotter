962f1477dbc362f1e486e2dec8344f5da3083dc0
Rednag <Weber.Benjamin@outlook.com>
2021-05-14 18:49:11 +0200

    Added chia determine batch

--- misc/det_times.bat
+++ misc/det_times.bat
@@ -2,4 +2,9 @@ set /p TEMP=Temporary Device :
 set /p TARGET=Target Device:
 set /p PLOTS=Amount of parallel plots:
 
-for /l %i in (1,1,%PLOTS) do start cmd /c chia plots create -k 32 -n 1 -b 3390 -r 2 -t %TEMP -d %TARGET
+FOR /L %%i IN (1,1,100) DO (
+	IF /I %%i LEQ %PLOTS% (
+		echo %%i
+		start cmd /c chia plots create -k 32 -n 1 -b 3390 -r 2 -t %TEMP% -d %TARGET%
+	)
+)
